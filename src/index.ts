6241a65a3a8c7e5620c704c525c06b6df28b5a0f
makedu963 <makedu963@gmail.com>
2025-06-19 13:10:22 +0530

    removed /

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: ["http://localhost:3000","https://homi-hunt.vercel.app/"],
+  origin: ["http://localhost:3000","https://homi-hunt.vercel.app"],
   credentials: true
 }));
 
